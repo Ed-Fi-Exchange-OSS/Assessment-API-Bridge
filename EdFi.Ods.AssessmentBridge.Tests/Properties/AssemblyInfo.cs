3a02bbdd228b21a187ec2d0def65c2f1beaf36d2
Vinaya <vinaya.mayya@ed-fi.org>
2021-01-23 21:31:07 -0600

    added license header

--- EdFi.Ods.AssessmentBridge.Tests/Properties/AssemblyInfo.cs
+++ EdFi.Ods.AssessmentBridge.Tests/Properties/AssemblyInfo.cs
@@ -1,4 +1,9 @@
-﻿using System.Reflection;
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+ 
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
--- EdFi.Ods.AssessmentBridge.WebApi/Common/Interfaces/Stubs/PipelineFactory.cs
+++ EdFi.Ods.AssessmentBridge.WebApi/Common/Interfaces/Stubs/PipelineFactory.cs
@@ -1,4 +1,9 @@
-﻿using EdFi.Ods.Pipelines.Factories;
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+ 
+using EdFi.Ods.Pipelines.Factories;
 
 namespace EdFi.Ods.AssessmentBridge.WebApi.Common.Interfaces.Stubs
 {
